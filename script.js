e441545ca66ddda1e304cf6eb2cffa5867e9733a
ayajojp <trumpet.jose.ayala@gmail.com>
2026-06-11 14:30:50 +0900

    Add two stories with dynamic variables in script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+let adjective =  "spiky";
+let noun = "lantern";
+let verb = "wander";
+let place = "forest";
+let adjective2 = "breezy";
+let noun2 = "teapot";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was "+ verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective =  "angry";
+noun = "table";
+verb = "jump";
+place = "street";
+adjective2 = "cold";
+noun2 = "pen";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was "+ verb + ".";
+
+console.log("Second story: " + secondStory);
+
